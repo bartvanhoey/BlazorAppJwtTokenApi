b585caa7799d8836388f3807505138d3d9e78234
bartvanhoey <bartvanhoey@hotmail.com>
2021-03-15 20:47:56 +0100

    uitestst update

--- T3App.UiTests/UnitTest1.cs
+++ T3App.UiTests/UnitTest1.cs
@@ -18,7 +18,7 @@ namespace T3App.UiTests
             options.AddArgument("--headless");
             driver = new ChromeDriver(options);
             appURL = Environment.GetEnvironmentVariable("TestUrl");
-            if (string.IsNullOrEmpty(appURL)) appURL = "https://localhost:44376/";
+            if (string.IsNullOrEmpty(appURL)) appURL = "https://localhost:44376";
             Console.WriteLine($"appURL is: {appURL}");
         }
 
@@ -44,9 +44,60 @@ namespace T3App.UiTests
             driver.Quit();
         }
 
+        // Not always working correctly
+        [Fact]
+        public void ValidCredentialsShouldLogin()
+        {
+            driver.Navigate().GoToUrl(appURL + "/login");
+
+            //var loginText = driver.FindElement(By.CssSelector("h1")).Text;
+            
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+
+            var webElement = wait.Until(c => c.FindElement(By.CssSelector("h1")));
+            Assert.Equal("Login", driver.FindElements(By.CssSelector("h1"))[0].Text);
+
+            wait.Until(c => c.FindElement(By.Id("email"))).SendKeys("admin");
+            wait.Until(c => c.FindElement(By.Id("password"))).SendKeys("SecurePassword");
+            //Assert.Equal("Current count: 0", driver.FindElements(By.CssSelector("p"))[0].Text);
+            driver.FindElement(By.CssSelector(".btn")).Click();
+            //Assert.Equal("Current count: 1", driver.FindElements(By.CssSelector("p"))[0].Text);
+
+            Assert.Equal("T3App.Blazor", driver.Title);
+            Assert.Contains("Hello, world!", driver.PageSource);
+
+            driver.Quit();
+        }
+
+        // Not always working correctly
+        [Fact]
+        public void InvalidCredentialsShouldNotLogin()
+        {
+            driver.Navigate().GoToUrl(appURL + "/login");
+
+            //var loginText = driver.FindElement(By.CssSelector("h1")).Text;
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+
+            var webElement = wait.Until(c => c.FindElement(By.CssSelector("h1")));
+            Assert.Equal("Login", driver.FindElements(By.CssSelector("h1"))[0].Text);
+
+            wait.Until(c => c.FindElement(By.Id("email"))).SendKeys("admin");
+            wait.Until(c => c.FindElement(By.Id("password"))).SendKeys("admin");
+            //Assert.Equal("Current count: 0", driver.FindElements(By.CssSelector("p"))[0].Text);
+            driver.FindElement(By.CssSelector(".btn")).Click();
+            //Assert.Equal("Current count: 1", driver.FindElements(By.CssSelector("p"))[0].Text);
+
+            Assert.Equal("T3App.Blazor", driver.Title);
+            Assert.Contains("Error logging you in", driver.PageSource);
+
+            driver.Quit();
+        }
+
 
         public void Dispose()
         {
+            driver.Quit();
             driver.Dispose();
         }
     }
